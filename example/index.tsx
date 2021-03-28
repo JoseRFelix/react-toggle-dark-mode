7d5d2e555e098b9f667f4a7685dd4936da25f97a
JoseRFelix <jfelx@outlook.com>
2021-03-28 17:27:38 -0400

    fix: add unique svg id

--- example/index.tsx
+++ example/index.tsx
@@ -3,11 +3,20 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { DarkModeSwitch } from '../.';
 
+function arrayN(size: number) {
+  return new Array(size).fill(undefined);
+}
+
 const App = () => {
   const [isDarkMode, setDarkMode] = React.useState(false);
+  const [toggleAmount, setToggleAmount] = React.useState(0);
 
   const toggleDarkMode = (checked: boolean) => {
     setDarkMode(checked);
+  };
+
+  const addToggle = () => {
+    setToggleAmount(prevValue => prevValue + 1);
   };
 
   return (
@@ -29,11 +38,27 @@ const App = () => {
         size={120}
       />
       <DarkModeSwitch
+        style={{ marginBottom: '2rem' }}
+        checked={isDarkMode}
+        onChange={toggleDarkMode}
+        size={80}
+      />
+      <DarkModeSwitch
+        style={{ marginBottom: '2rem' }}
         checked={isDarkMode}
         onChange={toggleDarkMode}
         moonColor="red"
         size={30}
       />
+      {arrayN(toggleAmount).map(() => (
+        <DarkModeSwitch
+          style={{ marginBottom: '2rem' }}
+          checked={isDarkMode}
+          onChange={toggleDarkMode}
+          size={Math.floor(Math.random() * 60) + 1}
+        />
+      ))}
+      <button onClick={addToggle}>Add toggle</button>
     </div>
   );
 };
