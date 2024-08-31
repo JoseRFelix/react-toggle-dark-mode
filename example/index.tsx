b32f5a8776de845c9102915cf50276c2691ddf47
Mo Beigi <me@mobeigi.com>
2024-08-31 10:59:50 +1000

    Prettify example code

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,6 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
-import { createRoot } from 'react-dom/client'; 
+import { createRoot } from 'react-dom/client';
 import { DarkModeSwitch, ThemeMode, DarkModeSwitchHandle } from '../.';
 
 export const customColors = {
@@ -19,9 +19,12 @@ export const customColors = {
 
 const App = () => {
   const [themeMode, setThemeMode] = React.useState(ThemeMode.System);
-  const [isSystemThemeModeEnabled, setIsSystemThemeModeEnabled] = React.useState(true);
+  const [
+    isSystemThemeModeEnabled,
+    setIsSystemThemeModeEnabled,
+  ] = React.useState(true);
   const darkModeSwitchRef = React.useRef<DarkModeSwitchHandle>(null);
-  
+
   const cycleThemeMode = (themeMode: ThemeMode) => {
     setThemeMode(themeMode);
   };
@@ -39,144 +42,96 @@ const App = () => {
     }
   };
 
-  const sizesToDemo = [200, 120, 80, 30]
+  const sizesToDemo = [200, 120, 80, 30];
 
   return (
     <div
       style={{
         display: 'flex',
         flexDirection: 'column',
-      }}>
-        <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '15vh',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#d9d9d9',
-      }}>
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '15vh',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#d9d9d9',
+        }}
+      >
         <h1>react-toggle-dark-mode</h1>
-      <div style={{
-        display: 'flex',
-        flexDirection: 'row',
-        gap: '1em',
-      }}>
-        <input
-          type="checkbox"
-          checked={isSystemThemeModeEnabled}
-          onChange={toggleisSystemThemeModeEnabled}
-        />
-        <label><strong>isSystemThemeModeEnabled</strong></label>
-        <button onClick={triggerClick}>Trigger Click</button>
-      </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '1em',
+          }}
+        >
+          <input
+            type="checkbox"
+            checked={isSystemThemeModeEnabled}
+            onChange={toggleisSystemThemeModeEnabled}
+          />
+          <label>
+            <strong>isSystemThemeModeEnabled</strong>
+          </label>
+          <button onClick={triggerClick}>Trigger Click</button>
         </div>
+      </div>
       <div
         style={{
           display: 'flex',
           flexDirection: 'row',
           minHeight: '85vh',
           overflow: 'hidden',
-        }}>
+        }}
+      >
+        <div
+          style={{
+            width: '50vw',
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '5em',
+            background: '#eee',
+          }}
+        >
           <div
             style={{
-              width: '50vw',
               display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
+              flexDirection: 'column',
               justifyContent: 'center',
-              gap: '5em',
-              background: '#eee',
+              alignItems: 'center',
+              gap: '2em',
             }}
           >
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '2em',
-              }}
-            >
-            <h2 style={{color: '#1b242c'}}>Default</h2>
-             {sizesToDemo.map((size) => 
-              (<DarkModeSwitch
+            <h2 style={{ color: '#1b242c' }}>Default</h2>
+            {sizesToDemo.map(size => (
+              <DarkModeSwitch
                 key={size}
                 ref={darkModeSwitchRef}
                 size={size}
                 onChange={cycleThemeMode}
                 isSystemThemeModeEnabled={isSystemThemeModeEnabled}
                 themeMode={themeMode}
-              />)
-             )}
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '2em',
-              }}
-            >
-            <h2 style={{color: '#1b242c'}}>Customised</h2>
-            {sizesToDemo.map((size) => 
-              (<DarkModeSwitch
-                key={size}
-                ref={darkModeSwitchRef}
-                size={size}
-                onChange={cycleThemeMode}
-                isSystemThemeModeEnabled={isSystemThemeModeEnabled}
-                themeMode={themeMode}
-                colors={customColors}
-              />)
-             )}
-            </div>
+              />
+            ))}
           </div>
           <div
             style={{
-              width: '50vw',
               display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
+              flexDirection: 'column',
               justifyContent: 'center',
-              gap: '5em',
-              background: '#1b242c',
+              alignItems: 'center',
+              gap: '2em',
             }}
           >
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '2em',
-              }}
-            >
-            <h2 style={{color: '#eee'}}>Default</h2>
-            {sizesToDemo.map((size) => 
-              (<DarkModeSwitch
-                key={size}
-                ref={darkModeSwitchRef}
-                size={size}
-                onChange={cycleThemeMode}
-                isSystemThemeModeEnabled={isSystemThemeModeEnabled}
-                themeMode={themeMode}
-              />)
-             )}
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '2em',
-              }}
-            >
-            <h2 style={{color: '#eee'}}>Customised</h2>
-            {sizesToDemo.map((size) => 
-              (<DarkModeSwitch
+            <h2 style={{ color: '#1b242c' }}>Customised</h2>
+            {sizesToDemo.map(size => (
+              <DarkModeSwitch
                 key={size}
                 ref={darkModeSwitchRef}
                 size={size}
@@ -184,12 +139,67 @@ const App = () => {
                 isSystemThemeModeEnabled={isSystemThemeModeEnabled}
                 themeMode={themeMode}
                 colors={customColors}
-              />)
-             )}
-            </div>
+              />
+            ))}
+          </div>
+        </div>
+        <div
+          style={{
+            width: '50vw',
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '5em',
+            background: '#1b242c',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: '2em',
+            }}
+          >
+            <h2 style={{ color: '#eee' }}>Default</h2>
+            {sizesToDemo.map(size => (
+              <DarkModeSwitch
+                key={size}
+                ref={darkModeSwitchRef}
+                size={size}
+                onChange={cycleThemeMode}
+                isSystemThemeModeEnabled={isSystemThemeModeEnabled}
+                themeMode={themeMode}
+              />
+            ))}
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: '2em',
+            }}
+          >
+            <h2 style={{ color: '#eee' }}>Customised</h2>
+            {sizesToDemo.map(size => (
+              <DarkModeSwitch
+                key={size}
+                ref={darkModeSwitchRef}
+                size={size}
+                onChange={cycleThemeMode}
+                isSystemThemeModeEnabled={isSystemThemeModeEnabled}
+                themeMode={themeMode}
+                colors={customColors}
+              />
+            ))}
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
